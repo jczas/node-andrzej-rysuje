ef37a4d661b9629dd230779ed04ecde8e0172b61
Jacek Czastkiewicz <jacek.czastkiewicz@capgemini.com>
2017-01-17 21:03:59 +0100

    finished

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,32 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-var request = require('request');
+const request = require('request');
 
-var xpath = require('xpath');
-var dom = require('xmldom').DOMParser;
+const xpath = require('xpath');
+const dom = require('xmldom').DOMParser;
 
-var RSS = require('rss');
+const RSS = require('rss');
 
 router.get('/', function (req, res, next) {
 
-  var url = 'http://www.andrzejrysuje.pl/wordpress/feed/';
+  const url = 'http://www.andrzejrysuje.pl/wordpress/feed/';
 
   request(url, function (error, response, xml) {
     if (!error) {
-      var doc = new dom().parseFromString(xml);
-      var select = xpath.useNamespaces({ 'content': 'http://purl.org/rss/1.0/modules/content/' });
-      var nodes = select('//content:encoded', doc);
-      console.log('nodes.length: ' + nodes.length);
+      const doc = new dom().parseFromString(xml);
+      const select = xpath.useNamespaces({ 'content': 'http://purl.org/rss/1.0/modules/content/' });
+      const nodes = select('//content:encoded', doc);
 
-      var feed = new RSS({
+      const feed = new RSS({
         title: 'Andrzej Rysuje',
         site_url: 'http://www.andrzejrysuje.pl',
         image_url: 'http://www.andrzejrysuje.pl/assets/img/logo.png',
         ttl: '5'
       });
 
-      var imgEx = /src="([^"]*)"/;
-      var altEx = /alt="([^"]*)"/;
+      const imgEx = /src="([^"]*)"/;
+      const altEx = /alt="([^"]*)"/;
 
       for (var i = 0, len = nodes.length; i < len; i++) {
         var data = nodes[i].firstChild.data;
@@ -35,15 +34,11 @@ router.get('/', function (req, res, next) {
         var altMatch = altEx.exec(data);
         var imgMatch = imgEx.exec(data);
         if (imgMatch !== null && altMatch !== null) {
-          console.log('img: ' + imgMatch[1]);
-          console.log('alt: ' + altMatch[1]);
-
           feed.item({
             title: altMatch[1],
             url: imgMatch[1]
           });
         }
-
       }
 
       res.set('Content-Type', 'text/xml');
