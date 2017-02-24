fd3737782dfb197303d4783c44230f5a49e3395b
Bartek Szczepanski <bartosz.szczepanski@capgemini.com>
2017-02-24 14:10:37 +0100

    finished

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
+var users = require('./routes/users');
+var rss = require('./routes/rss');
 
 var app = express();
 
@@ -13,6 +15,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,6 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/users', users);
+app.use('/rss', rss);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/rss.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var dom = require('xmldom').DOMParser;
+var xpath = require('xpath');
+var RSS = require('rss');
+
+router.get('/', function (req, res, next) {
+
+  var url = 'http://www.andrzejrysuje.pl/wordpress/feed/';
+
+  request(url, function (error, response, xml) {
+    if (!error) {
+      var doc = new dom().parseFromString(xml);
+      var select = xpath.useNamespaces({ 'content': 'http://purl.org/rss/1.0/modules/content/' });
+      var nodes = select('//content:encoded', doc);
+      console.log('nodes.length: ' + nodes.length);
+
+      var feed = new RSS({
+        title: 'Andrzej Rysuje',
+        site_url: 'http://www.andrzejrysuje.pl',
+        image_url: 'http://www.andrzejrysuje.pl/assets/img/logo.png',
+        ttl: '5'
+      });
+
+      var imgEx = /src="([^"]*)"/;
+
+      for (var i = 0, len = nodes.length; i < len; i++) {
+        var data = nodes[i].firstChild.data;
+
+        var imgMatch = imgEx.exec(data);
+        if (imgMatch !== null) {
+          console.log('img: ' + imgMatch[1]);
+
+          feed.item({
+            title: '',
+            url: imgMatch[1]
+          });
+        }
+      }
+
+      res.set('Content-Type', 'text/xml');
+      res.send(feed.xml());
+    } else {
+      res.status(404);
+      res.send('Problem with ' + url);
+    }
+  });
+});
+
+module.exports = router;
